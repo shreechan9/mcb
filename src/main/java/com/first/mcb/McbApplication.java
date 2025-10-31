05068933474fb137cb577d7ba9915fe7c87823e9
Shreechan Byanju <shreechan@techmind.com.np>
2025-10-31 15:08:43 +0545

    chores: setup

---
+++ src/main/java/com/first/mcb/McbApplication.java
@@ -0,0 +1,13 @@
+package com.first.mcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/mcb/McbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.mcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
